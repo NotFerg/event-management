9918ddc30fcb3727ef74e5195e18e70de3ba9a1f
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-08-18 20:44:37 +0800

    chore: Refactor EventController to use Event model and return all events

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+// use App\Models\Event;
 
 class EventController extends Controller
 {
@@ -12,7 +13,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        //
+        return \App\Models\Event::all();
     }
 
     /**
@@ -26,9 +27,9 @@ class EventController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(\App\Models\Event $event) //$event is a parameter name that gets the id from the url, then it searches the event model with the id from the url
     {
-        //
+        return $event;
     }
 
     /**
