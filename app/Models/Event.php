15c5c5890b2a1323132bd172766aa0af4b614c42
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-12-25 17:27:36 +0800

    Add Laravel Sanctum for API authentication and implement Event and Attendee models with relationships

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\User;
+use App\Models\Attendee;
 
 class Event extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo{
+        return $this->belongsTo(User::class);
+    }
+
+    public function attendees(): HasMany{
+        return $this->hasMany(Attendee::class);
+    }
 }
--- database/migrations/2024_12_25_092244_create_personal_access_tokens_table.php
+++ database/migrations/2024_12_25_092244_create_personal_access_tokens_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 
 return new class extends Migration
 {
@@ -12,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('events', function (Blueprint $table) {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class);
+            $table->morphs('tokenable');
             $table->string('name');
-            $table->text('description')->nullable();
-            $table->dateTime('start_time');
-            $table->dateTime('end_time');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamp('expires_at')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('events');
+        Schema::dropIfExists('personal_access_tokens');
     }
 };
