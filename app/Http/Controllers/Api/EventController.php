b9e4f1fdedcfbd3727424c9d83716d66bb588259
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2025-01-23 20:34:00 +0800

    Refactor EventController to use resource responses and add Event and Attendee resources

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use App\Http\Resources\EventResource;
 
 class EventController extends Controller
 {
@@ -13,7 +14,10 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        // return Event::all();
+        // return EventResource::collection(Event::all());
+        return EventResource::collection(Event::with('user')->get());
+        
     }
 
     /**
@@ -31,7 +35,8 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        // return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -39,7 +44,9 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        // return $event;
+        $event->load('user','attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -56,7 +63,8 @@ class EventController extends Controller
             ]),
         );
 
-        return $event;
+        // return $event;
+        return new EventResource($event);
     }
 
     /**
